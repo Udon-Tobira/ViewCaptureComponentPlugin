707c9675050a3be833efab3ed48d6a09ae69fe79
Udon-Tobira <146440502+Udon-Tobira@users.noreply.github.com>
2024-08-07 16:54:21 +0900

    move ExecuteSystemCommand to another plugin (submodule)

--- Source/ViewCaptureComponent/Private/AsyncExecTask.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "AsyncExecTask.h"
-
-FAsyncExecTask::FAsyncExecTask(TFunction<void()> InTask) : Task(InTask) {}
-
-void FAsyncExecTask::DoWork() {
-	Task();
-}
